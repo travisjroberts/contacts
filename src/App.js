e2b145c5e837270951e4687475d4abd4e355990e
Travis John Roberts <contact@travisjroberts.com>
2018-04-24 21:35:11 -0500

    added ListContacts.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import ListContacts from './ListContacts'
+
+const contacts = [
+  {
+    id: 'tyler',
+    name: 'Tyler McGinnis',
+    handle: '@tylermcginnis',
+    avatarURL: 'http://localhost:5001/tyler.jpg'
+  },
+  {
+    id: 'karen',
+    name: 'Karen Isgrigg',
+    handle: '@karen_isgrigg',
+    avatarURL: 'http://localhost:5001/karen.jpg'
+  },
+  {
+    id: 'richard',
+    name: 'Richard Kalehoff',
+    handle: '@richardkalehoff',
+    avatarURL: 'http://localhost:5001/richard.jpg'
+  },
+]
 
 class App extends Component {
   render() {
     return (
         <div>
-          Hello World
+          <ListContacts contacts={contacts} />
         </div>
     )
   }
